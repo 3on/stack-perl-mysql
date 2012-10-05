453ade37429a63ce7b13f5bed00b9d98a07e92be
Jr <jr@ailleurs.me>
2012-10-04 18:32:22 -0700

    now with a postintall script to create a db

--- app/app.pl
+++ app/app.pl
@@ -4,31 +4,26 @@ use JSON;
 use DBI;
 use DBD::mysql;
 
-
 my $envFilename = "/home/dotcloud/environment.json";
 my ($user, $password, $host, $port, $env, $webPort);
 
 if ( -e $envFilename ) {
     open my $fh, "<", $envFilename or die $!;
     $env = JSON::decode_json(join '', <$fh>);
-
     $user = $env->{DOTCLOUD_DB_MYSQL_LOGIN};
     $password = $env->{DOTCLOUD_DB_MYSQL_PASSWORD};
     $host = $env->{DOTCLOUD_DB_MYSQL_HOST};
     $port = $env->{DOTCLOUD_DB_MYSQL_PORT};
-
     $webPort = $env->{PORT_WWW};
 }
 else {
-    # local config
+    # local development environment
     $user = "root";
     $password = "root";
     $host = "localhost";
     $port = "3306";
-
     $webPort = "8080";
 }
-
 
 my $database = 'test';
 my $dbh = DBI->connect("DBI:mysql:database=$database;host=$host;port=$port", $user, $password) or 
@@ -38,10 +33,7 @@ app->config(hypnotoad => {listen => ['http://*:' . $webPort]});
 
 get '/' => sub {
     my $self = shift;
-
     $self->render(text => 'Hello World! from Mojolicious!');
 };
 
 app->start;
-
-# dotcloud run db echo "CREATE DATABASE test;" | mysql
--- app/mysql.pl
+++ app/mysql.pl
@@ -2,17 +2,38 @@
 use JSON;
 use DBI;
 use DBD::mysql;
+use IO::Handle;
 
-open my $fh, "<", "/home/dotcloud/environment.json" or die $!;
-my $env = JSON::decode_json(join '', <$fh>);
-
-
+my $envFilename = "/home/dotcloud/environment.json";
+my ($user , $password, $host, $port);
 my $database = 'test';
-my $user = $env->{DOTCLOUD_DB_MYSQL_LOGIN};
-my $password = $env->{DOTCLOUD_DB_MYSQL_PASSWORD};
-my $host = $env->{DOTCLOUD_DB_MYSQL_HOST};
-my $port = $env->{DOTCLOUD_DB_MYSQL_PORT};
-my $dbh = DBI->connect("DBI:mysql:hostname=$host;port=$port;", $user, $password) or 
-    die "Couldn't connect to database: " . DBI->errstr;
+my $dbh;
 
-DBI->exec("CREATE DATABASE IF NOT EXISTS $database;");
+if ( -e $envFilename ) {
+    # on dotCloud
+    open my $fh, "<", $envFilename;
+    my $env = JSON::decode_json(join '', <$fh>);
+
+    $user = $env->{DOTCLOUD_DB_MYSQL_LOGIN};
+    $password = $env->{DOTCLOUD_DB_MYSQL_PASSWORD};
+    $host = $env->{DOTCLOUD_DB_MYSQL_HOST};
+    $port = $env->{DOTCLOUD_DB_MYSQL_PORT};
+} else {
+    # local development environment
+    $user = "root";
+    $password = "root";
+    $host = "localhost";
+    $port = "3306";
+}
+
+print "Creating database '$database'..";
+
+for($tries = 0; $tries <= 30; $tries++) {
+    STDOUT->printflush(".");
+    $dbh = DBI->connect("DBI:mysql:hostname=$host;port=$port;", $user, $password, {RaiseError=>0,PrintError=>0});
+    last if(defined $dbh);
+    sleep(1);
+}
+print "\n";
+
+$dbh->func('createdb', $database, 'admin');
